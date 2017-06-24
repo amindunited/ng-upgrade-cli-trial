73b7686fef89b0d50a8eae475d73602ac880ac86
Robin Buckley <robin.buckley@auspost.com.au>
2017-06-24 16:44:14 +1000

    chore(setup): bootstrap the legacy app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { UpgradeModule } from '@angular/upgrade/static';
+import { NgOneModule } from './app/legacy/app';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.body, [NgOneModule.name], {strictDi: true});
+});
