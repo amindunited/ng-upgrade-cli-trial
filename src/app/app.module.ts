76f898325d79d86fa9f24b928d0c98b698089f4d
Robin Buckley <robin.buckley@auspost.com.au>
2017-06-24 16:43:29 +1000

    chore(setup): Add the upgraded legacy app to the app module and override the bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UpgradeModule } from '@angular/upgrade/static';
 
+//This is the component that we will create to wrap our old app
+import { UpgradedNgOneComponent } from './legacy/upgraded-app';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpgradedNgOneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  //adding upgrade to the appModule could allow it to be ref'd
+  constructor(public upgrade: UpgradeModule) {}
+  ngDoBootstrap() {}
+}
